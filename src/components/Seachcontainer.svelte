920ab25587bce9257b672205c5c4c2fa601a14a3
Maurialva <mauri.mdp87@gmail.com>
2021-05-15 20:45:50 -0300

    falta seleccionar butacas

--- src/components/Seachcontainer.svelte
+++ src/components/Seachcontainer.svelte
@@ -8,7 +8,7 @@ import Dropbutton from './Dropbutton.svelte';
     <div class="w-full sm:w-9/12 rounded-full border-solid pl-10 bg-gradient-to-r from-blue-400 via-blue-500 to-indigo-800 hover:from-indigo-400 via-indigo-500 to-blue-800 transition ease-in duration-700 ">       
          <form   action="busqueda">
              <br>
-                        <input type="text" class="w-4/5 transition ease-in duration-700 hover:bg-indigo-200 text-center text-white placeholder-white  bg-blue-200 rounded-full border-solid" placeholder="BUSCAR..." name="searchbar"  >
+                        <input type="text" class="w-4/5 transition ease-in duration-700 hover:bg-indigo-200 text-center text-white placeholder-white  bg-blue-200 rounded-full border-solid" placeholder="BUSCAR..." value="" name="searchbar"  >
                          <div class="text-right float-right 2xl:right-10 pb-8 scale-125 2xl:mr-14 2xl:-mt-4 -mt-4 hidden lg:block ">
                           <Searchicon/>
                        </div>
